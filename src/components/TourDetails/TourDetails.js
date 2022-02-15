5e2780e92fa0b8d37c0d96be7840709d9db03219
ayat93a <alkayedayat93@gmail.com>
2022-02-16 01:32:33 +0200

    17 v2

--- src/components/TourDetails/TourDetails.js
+++ src/components/TourDetails/TourDetails.js
@@ -1,14 +1,29 @@
 import { useParams } from "react-router-dom";
 import {Link} from 'react-router-dom'
+import { useState } from "react";
+
+const [readMore , setReadMore] = useState (false)
 
 function TourDetails (props){
     let {id} = useParams();
     const result = props.data.filter((value)=> value.id === {id});
     return (
         <>
-        <h2>{result[0]}</h2>
-        <Link to = '/'>Home </Link>
-
+        <h2>{result[0].name}</h2>
+        <p>
+            Description : {result[0].info}
+        
+            <button onClick = {() => setReadMore(!readMore)} >
+            {readMore ? 'Show More' : 'Show Less'}
+            
+            
+            </button>
+           
+        
+        </p>
+        <p>{result[0].price}</p>
+        <img src = '{result[0].image}' />
+    
         </>
     )
 }
